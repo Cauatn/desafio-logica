41f949608683acc23b5c2d52a1bc976926a59bd8
Cauã Tavares <tavarescauac@gmail.com>
2025-04-10 20:51:52 -0300

    chore: react-router impl

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Tableau from "./pages/Tableux/page.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import PropositionalLogicAnalyzer from "./pages/Home/page.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PropositionalLogicAnalyzer />}></Route>
+        <Route path="/tableau" element={<Tableau />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
